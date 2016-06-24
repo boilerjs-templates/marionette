1024642fe40489910d61023cc6db71c5d646e947
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2016-06-24 15:51:41 -0300

    templates and helpers organization.

--- application/helpers/config.js
+++ application/helpers/config.js
@@ -1,9 +1,5 @@
 var Handlebars = require('handlebars/runtime');
 
 Handlebars.registerHelper('config', function(value) {
-	if(value === 'ENV') {
-		return new Handlebars.SafeString('<span style="color:tomato;">' + Config[value] + '</span>');
-	} else {
-		return Config[value];
-	}
+	return Config[value];
 });
